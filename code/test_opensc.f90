be6d83178667225dfb923538d2f7f397f7da3f30
Robert Ryne <rdryne@lbl.gov>
2017-11-07 20:01:14 -0800

    inserted domain decomposition code in main()

--- code/test_opensc.f90
+++ code/test_opensc.f90
@@ -38,7 +38,8 @@ integer :: mprocs,myrank,ierr
 namelist / opensc_test_params / &
     nx, ny, nz, n_particle, e_tot, bunch_charge, &
     sigma_x, sigma_y, sigma_z, gaussiancutoff, &
-    direct_field_calc, integrated_green_function
+    direct_field_calc, integrated_green_function, &
+    idecomp, npx, npy, npz
 !
 call MPI_INIT(ierr)      !initialize MPI
 call MPI_COMM_SIZE(MPI_COMM_WORLD,mprocs,ierr)
@@ -59,6 +60,10 @@ gaussiancutoff=5
 disttype=1 ! =0 for uniform, =1 for Gaussian
 direct_field_calc = .true.  
 integrated_green_function = .true.
+idecomp=0
+npx=1
+npy=1
+npz=1
  
 !Read namelist
 in_file = 'test_opensc.in'
@@ -83,11 +88,43 @@ allocate(y(n_particle,n1))
 
 
 ! parameters needed to call openbc routines:
+! global sizes:
 ilo_rho_gbl=1; ihi_rho_gbl=nx; jlo_rho_gbl=1; jhi_rho_gbl=ny; klo_rho_gbl=1; khi_rho_gbl=nz !assumes power-of-2
-ilo=ilo_rho_gbl;ihi=ihi_rho_gbl; jlo=jlo_rho_gbl;jhi=jhi_rho_gbl; klo=klo_rho_gbl;khi=khi_rho_gbl !serial code
-if(myrank.eq.0)write(6,*)'grid sizes ihi,jhi,khi=',ihi,jhi,khi
-idecomp=-1;npx=1;npy=1;npz=1 !none of these are relevant to this serial code
-!
+
+! determine local array sizes:
+if(idecomp.lt.-1.or.idecomp.gt.6)then
+  if(myrank.eq.0)write(6,*)'error: idecomp=',idecomp
+  stop
+endif
+
+! if idecomp=0,...,6 then idecomp defines the type of domain decomposition:  0=xyz, 1=xy, 2=yz, 3=xz, 4=x, 5=y, 6=z,
+! and the code will determine npx,npy,npz. But if idecomp=-1 then the user has specified npx,npy,npz on input
+if(idecomp.ge.0.and.idecomp.le.6)then
+  call procgriddecomp(mprocs,ilo_rho_gbl,ihi_rho_gbl,jlo_rho_gbl,jhi_rho_gbl,klo_rho_gbl,khi_rho_gbl,idecomp,npx,npy,npz)
+  if(npx*npy*npz.ne.mprocs)then
+    if(myrank.eq.0)write(6,*)'error: npx*npy*npz must equal mprocs but in this case npx,npy,npz,mprocs=',npx,npy,npz,mprocs
+    stop
+  endif
+endif
+if(myrank.eq.0)then
+  write(6,*)'running with ',mprocs,' MPI processes'
+  write(6,*)'global grid imin,imax=',ilo_rho_gbl,ihi_rho_gbl
+  write(6,*)'global grid jmin,jmax=',jlo_rho_gbl,jhi_rho_gbl
+  write(6,*)'global grid kmin,kmax=',klo_rho_gbl,khi_rho_gbl
+  write(6,*)'grid domain decomposition:'
+  write(6,*)'idecomp=',idecomp
+  write(6,*)'# of processes in the x-direction =',npx
+  write(6,*)'# of processes in the y-direction =',npy
+  write(6,*)'# of processes in the z-direction =',npz
+endif
+
+! Important: the openBC routine assume that local arrays have indices that are subsets of the global indices.
+! In other words, local arrays are NOT dimensioned (1:nxlcl,1:nylcl,1:nzlcl).
+! The user must supply, as arguments to the routine, the starting and ending indices of local arrays in the GLOBAL system.
+! The following call to routine decompose returns the required quantities, namely, ilo,ihi,jlo,jhi,klo,khi
+call decompose(myrank,mprocs,&
+     ilo_rho_gbl,ihi_rho_gbl,jlo_rho_gbl,jhi_rho_gbl,klo_rho_gbl,khi_rho_gbl,idecomp,npx,npy,npz,ilo,ihi,jlo,jhi,klo,khi)
+ 
 ! initialize the particles:
 if(myrank.eq.0)write(6,*)'gamma=',gamma
 sigmat(1:6,1:6)=0.d0
@@ -109,6 +146,7 @@ endif
 if(myrank.eq.0)write(2,'(7(1pe12.5,1x))')y(1,1:7),y(2,1:7),y(3,1:7),y(4,1:7),y(5,1:7)
 if(myrank.eq.1)write(3,'(7(1pe12.5,1x))')y(1,1:7),y(2,1:7),y(3,1:7),y(4,1:7),y(5,1:7)
 if(myrank.eq.2)write(4,'(7(1pe12.5,1x))')y(1,1:7),y(2,1:7),y(3,1:7),y(4,1:7),y(5,1:7)
+if(npx.ne.12345)stop !debuggin, so stop here
 !
 
 if(myrank.eq.0)print *, 'depositing bunch on mesh...'
